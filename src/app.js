f73b521fb1c54f1b3fede1c8de564e93556e35f3
Lucas Camargo <lucascam_8@hotmail.com>
2022-01-13 21:04:03 -0300

    desafio

--- src/app.js
+++ src/app.js
@@ -50,8 +50,8 @@ io.on('connection', socket => {
 })
 
 app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
 app.set('views', './views')
+app.set('view engine', 'handlebars')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
