b0c148ebdf7d4f5383229cf6f0ebc9c58d7bf6ff
Lucas Camargo <lucascam_8@hotmail.com>
2022-01-13 21:26:56 -0300

    desafio

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import path from 'path'
 import { generate, __dirname } from './utils.js'
 import { engine } from 'express-handlebars'
 import { Server } from 'socket.io'
@@ -50,7 +51,7 @@ io.on('connection', socket => {
 })
 
 app.engine('handlebars', engine())
-app.set('views', './views')
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'handlebars')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
